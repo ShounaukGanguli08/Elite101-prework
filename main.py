90e7e2931e77ee97365c6e95d635dde89d57abd2
ShounaukGanguli08 <shounaukganguli@gmail.com>
2024-09-18 16:59:47 +0000

    made 1st version of chatbot

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World!!!!!!!!!!!")
+print("Hello User")
+name = input( "What is your name?")
+age = input(f"{name}, What is your age?")
+menuiput = input("What can I do for you?\n[1] [2] [3] [4] Exit")
+if menuiput == "4":
+  print("Exited program")
+
+
+
